4801bdc0214788efdd924a76d7a843448332ca3f
Мишланов <mishlan0v@mail.ru>
2025-03-04 13:24:49 +0500

    Чутка изменен дизайн входа и рега

--- obj/Debug/net8.0/CampManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/CampManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3acf0d6004b3b2315a5994ca9706d012b9975ad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
